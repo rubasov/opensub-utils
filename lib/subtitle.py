8ccc76ec281ea990382c2840e0a4f8dea9010bf7
Bence Romsics <rubasov@gmail.com>
2013-02-02 16:00:06 +0100

    Delete unused code.

--- lib/subtitle.py
+++ lib/subtitle.py
@@ -24,25 +24,6 @@ def path_has_subtitle_extension(path):
 
     return is_recognized
 
-def possible_subtitle_paths(movie_path):
-    base, extension = os.path.splitext(movie_path)
-
-    assert extension != "", (
-        "movie path has no extension: {}".format(movie_path) )
-
-    path_set = set()
-    for sub_ext in recognized_subtitle_extensions():
-        path_set.add(base + sub_ext)
-
-    return path_set
-
-def movie_has_subtitle(movie_path):
-    rv = any(
-        map(
-            os.path.isfile,
-            possible_subtitle_paths(movie_path) ) )
-    return rv
-
 class Subtitle(object):
 
     def __init__( self, name, content ):

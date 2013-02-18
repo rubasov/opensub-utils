262122d11ebc13db6521558eba595f36248c2314
Bence Romsics <rubasov@gmail.com>
2013-02-18 16:17:02 +0100

    Turn test documentation into a script.

--- src/test/run.sh
+++ src/test/run.sh
@@ -1,21 +1,55 @@
-# FIXME comment this file
+#! /bin/sh
 
-sudo apt-get install polipo
+# quick-and-dirty test runner script
 
-cd opensub-utils/src/
+if test $# = 0
+then
 
-find . -type f \( -name '*.py' -o -name 'opensub-*' \) | xargs pep8
+cat >&2 <<EOF
+usage: $0 <movie_cd1.avi> <movie_cd2.avi> ...
 
-wget -O test/test-data/breakdance.avi ...
+It is recommended to test with a multi-cd movie.
+
+It is a good idea to use a caching http proxy to avoid hitting the
+daily request limit on opensubtitles.org due to testing. For example:
+
+$ sudo apt-get install polipo # it listens at 127.0.0.1:8123
+
+You may also want to populate the cache, before running this script:
+
+http_proxy=127.0.0.1:8123 ./bin/opensub-get -vv -t - >/dev/null "$@"
+EOF
+
+exit 1
+fi
+
+cd "$( dirname "$0" )"/..
+
+test_avi_path="test/test-data/breakdance.avi"
+if ! test -f "$test_avi_path"
+then
+    wget \
+        --tries=1 \
+        --no-clobber \
+        --output-document="$test_avi_path" \
+        "http://www.opensubtitles.org/addons/avi/breakdance.avi"
+fi
+
+find . -type f \( -name "*.py" -o -name "opensub-*" \) | xargs pep8
 
 ls test/test_* | xargs -n1 python
 ls test/test_* | xargs -n1 python3
 
-python bin/opensub-hash test/test-data/breakdance.avi
-python3 bin/opensub-hash test/test-data/breakdance.avi
+python bin/opensub-hash "$test_avi_path"
+python3 bin/opensub-hash "$test_avi_path"
 
-# populate cache
-http_proxy=127.0.0.1:8123 ./bin/opensub-get -vv -t /dev/null -f some_multi_cd_movie*.avi
+export http_proxy=127.0.0.1:8123
 
-magic=more http_proxy=127.0.0.1:8123 python bin/opensub-get -vv -t - >/dev/null some_multi_cd_movie*.avi
-magic=more http_proxy=127.0.0.1:8123 python3 bin/opensub-get -vv -t - >/dev/null some_multi_cd_movie*.avi
+python bin/opensub-get -vv -t - >/dev/null "$@"
+python3 bin/opensub-get -vv -t - >/dev/null "$@"
+
+tmpdir="$( mktemp -d )"
+python bin/opensub-get -vv -t "$tmpdir/test-{num}.{subtitle/ext}" "$@"
+python3 bin/opensub-get -vv -t "$tmpdir/test3-{num}.{subtitle/ext}" "$@"
+rm -f "$tmpdir"/test*
+rmdir "$tmpdir"

0d0d9c2e4d5a32bfa0d459d83393ddf8891a7e93
Bence Romsics <rubasov@gmail.com>
2013-03-07 16:08:10 +0100

    Remove unused test runner script.

--- src/test/test_get.py
+++ src/test/test_get.py
@@ -1,12 +1,5 @@
-#! /bin/sh
-
-# quick-and-dirty test runner script
-
-if test $# = 0
-then
-
-cat >&2 <<EOF
-usage: $0 <movie_cd1.avi> <movie_cd2.avi> ...
+"""
+TODO test opensub-get
 
 It is recommended to test with a multi-cd movie.
 
@@ -18,18 +11,10 @@ $ sudo apt-get install polipo # it listens at 127.0.0.1:8123
 You may also want to populate the cache, before running this script:
 
 http_proxy=127.0.0.1:8123 ./bin/opensub-get -vv -t - >/dev/null ...
-EOF
-
-exit 1
-fi
-
-cd "$( dirname "$0" )"/..
-
-ls test/test_* | xargs -n1 python
-ls test/test_* | xargs -n1 python3
-
+ 
 export http_proxy=127.0.0.1:8123
 export http_cache_control="only-if-cached"
 
 python bin/opensub-get -vv -t - >/dev/null -- "$@"
 python3 bin/opensub-get -vv -t - >/dev/null -- "$@"
+"""

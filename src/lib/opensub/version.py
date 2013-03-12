0fe2dedf080d8b07d1cbbd16253dffb909f52206
Bence Romsics <rubasov@gmail.com>
2013-03-12 15:40:21 +0100

    Update version file.

--- src/lib/opensub/version.py
+++ src/lib/opensub/version.py
@@ -1,6 +1,21 @@
-"""This file is templated at release time."""
+"""
+Version of the opensub-utils distribution.
 
-__version__ = "[% version %]"
+  * Keep _one_ version number for the whole distribution.
+    Don't start versioning the scripts, modules, etc separately.
+
+  * Use Semantic Versioning v2 as a guide: http://semver.org/
+
+  * Don't repeat the version in the commit message.
+
+  * Tag the commit changing this file.
+"""
+
+# DO NOT FORGET TO TAG:
+# python lib/opensub/version.py | xargs -r -I {} git tag -a {} -m {}
+
+__version_info__ = ("0", "9", "3")
+__version__ = ".".join(__version_info__)
 
 if __name__ == "__main__":
 
